bcde2406bbbbe27896c80930cfdd167b08d5cf52
new5558 <new17353@gmail.com>
2019-01-11 03:47:45 +0700

    change countdown to event date from endDate to startDate && added start of registration date banner

--- components/home/local-components/UrgentAnnouncement.jsx
+++ components/home/local-components/UrgentAnnouncement.jsx
@@ -10,6 +10,7 @@ class UrgentAnnouncement extends Component {
     const startDate = moment(commonsData.startDate);
     const endDate = moment(commonsData.endDate);
     const regisEndDate = moment(commonsData.registrationEndDate);
+    const regisStartDate = moment(commonsData.registrationStartDate);
     const now = moment();
     const startAndEndMonth = startDate.format('M') === endDate.format('M') 
       ? startDate.format('MMMM') 
@@ -21,12 +22,12 @@ class UrgentAnnouncement extends Component {
         <div className="flex flex-col sm:flex-row py-6 justify-center sm:items-center font-cu-heading text-white">
           <div className=" sm:pb-0 sm:pr-4 md:pr-8 mb-3 sm:mb-0 flex justify-start sm:justify-end">
             {this.contentDate(startDate.format('D') + "-" + endDate.format('D'), startAndEndMonth)}
-            {this.contentDesc(t('donationDate'), `${t('left')} ${endDate.diff(now, 'days')} ${t('days')}`)}
+            {this.contentDesc(t('donationDate'), `${t('left')} ${startDate.diff(now, 'days')} ${t('days')}`)}
           </div>
           <div className="sm:border-l border-t w-full sm:w-px sm:h-16 special-color"></div>
           <div className=" sm:pl-4 md:pl-8 mt-3 sm:mt-0 flex items-end">
-            {this.contentDate(regisEndDate.format('D'), regisEndDate.format('MMMM'))}
-            {this.contentDesc(t('endOfRegistrationDate'), `${t('left')} ${regisEndDate.diff(now, 'days')} ${t('days')}`)}
+            {regisStartDate > now ? this.contentDate(regisStartDate.format('D'), regisStartDate.format('MMMM')) : this.contentDate(regisEndDate.format('D'), regisEndDate.format('MMMM'))}
+            {regisStartDate > now ? this.contentDesc(t('startOfRegistrationDate'), `${t('left')} ${regisStartDate.diff(now, 'days')} ${t('days')}`) : this.contentDesc(t('endOfRegistrationDate'), `${t('left')} ${regisEndDate.diff(now, 'days')} ${t('days')}`)}
           </div>
         </div>
       </div>

744eefe28a7494d476e12bad3d96cf589ee7567b
Poom K <pattarapon.kitti@gmail.com>
2019-01-15 21:56:44 +0700

    add such that only the last 4 stats will show

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React, { Component } from 'react';
 import Statistic from "./Component/Statistic";
 import axios from './core/core';
-<<<<<<< HEAD
-=======
-import Navbar from './component/Navbar';
->>>>>>> 47c6bed8224779eb6c5dd83d8f79f980aafa1eb1
+import Navbar from './Component/Navbar';
 
 class App extends Component {
 
   render() {
-<<<<<<< HEAD
-     return (
-      <div className="font-sans border-black">
-=======
     return (
       <div className="font-sans border-black">  
         <Navbar />
->>>>>>> 47c6bed8224779eb6c5dd83d8f79f980aafa1eb1
         <Statistic />
         <div style={{height:'2000px', background: 'linear-gradient(to right bottom, #430089, #82ffa1)'}}>
         </div>
--- src/Component/Statistic.js
+++ src/Component/Statistic.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from "../core/core";
+import StatCont from "./StatCont";
 
 class Statistic extends Component {
     constructor(props) {
@@ -10,7 +11,10 @@ class Statistic extends Component {
         }
     }
     async getSomeData() {
-        return await axios.get('./posts?_page=0&_limit=4')
+        return await axios.get('./posts')
+        .then(response => response.data.length)
+        .then(dataNumber => axios.get(`./posts?_start=${dataNumber-4}&_limit=4`));
+        // return await axios.get('./posts?_page=0&_limit=4')
     }
 
     componentDidMount() {
@@ -27,32 +31,30 @@ class Statistic extends Component {
             return <div></div>
         } 
         
-        // let a = this.state.data.map((element => {
-        //     <Card name={element.title} number={element.id}/>
-        // }))
+        let content = this.state.data.map(element => {
+        let a = [];
+        a.push(<StatCont number={element.id} content={element.title}/>);
+        return a;
+        });
 
         return (
-            <div className="container border-solid border-2 border-black flex flex-col items-center font-cu-heading">
+            <div className="flex flex-col items-center font-cu-heading">
                 <div className="border-sold border-red-dark border-b-8 text-center mt-10 mb-10" >
-                    <h2 className="text-lg mb-3 text-pink">STATISTICS</h2>
+                    <h2 className="text-base tracking-wide mb-3 text-pink">STATISTICS</h2>
                     <h1 className="text-3xl mb-3">ข้อมูลทางสถิติ {}</h1> 
                 </div>
                 <div className="flex flex-wrap w-3/5 mb-10">
                     <div className="border-grey border-r border-b border-solid text-center w-1/2 pt-5 pb-10 pr-10 pl-10">
-                        <h2 className="text-xl pb-10 text-5xl text-red-dark">2,593</h2>
-                        <p className="text-2xl">ผู้ลงทะเบียนบริจาคเลือด ปี พ.ศ. 2561</p>
+                        {content[0]};
                     </div>
                     <div className="border-grey border-b border-solid text-center w-1/2 pt-5 pb-10 pr-10 pl-10">
-                        <h2 className="text-xl pb-10 text-5xl text-red-dark">1,000</h2>
-                        <p className="text-2xl">ชั่วโมงการทำงานของ Thinc. Development</p>
+                        {content[1]};
                     </div>
                     <div className="border-grey border-r border-solid text-center w-1/2 pt-10 pr-10 pl-10">
-                        <h2 className="text-xl pb-10 text-5xl text-red-dark">3</h2>
-                        <p className="text-2xl">ผู้มาเข้าร่วมประชุมตอนเก้าโมงเช้า</p>
+                        {content[2]};
                     </div>
                     <div className="text-center w-1/2 pt-10 pr-10 pl-10">
-                        <h2 className="text-xl pb-10 text-5xl text-red-dark">555</h2>
-                        <p className="text-2xl">ตามใจฉัน</p>
+                        {content[3]};
                     </div>
                 </div>
             </div>

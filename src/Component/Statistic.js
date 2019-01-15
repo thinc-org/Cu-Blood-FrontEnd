fab8f63b31bf0a6e56605c6f3a9bcb10d2360efc
Poom K <pattarapon.kitti@gmail.com>
2019-01-15 23:36:31 +0700

    add media query to Stat

--- src/Component/Statistic.js
+++ src/Component/Statistic.js
@@ -37,6 +37,8 @@ class Statistic extends Component {
         return a;
         });
 
+        let contentStyle = "border-grey border-solid border-b w-full sm:w-1/2 text-center pb-8 pr-10 pl-10";
+
         return (
             <div className="flex flex-col items-center font-cu-heading">
                 <div className="border-sold border-red-dark border-b-8 text-center mt-10 mb-10" >
@@ -44,16 +46,16 @@ class Statistic extends Component {
                     <h1 className="text-3xl mb-3">ข้อมูลทางสถิติ {}</h1> 
                 </div>
                 <div className="flex flex-wrap w-3/5 mb-10">
-                    <div className="border-grey border-r border-b border-solid text-center w-1/2 pt-5 pb-10 pr-10 pl-10">
+                    <div className={`${contentStyle} sm:border-r sm:pt-5`}>
                         {content[0]}
                     </div>
-                    <div className="border-grey border-b border-solid text-center w-1/2 pt-5 pb-10 pr-10 pl-10">
+                    <div className={`${contentStyle} pt-8 sm:pt-5`}>
                         {content[1]}
                     </div>
-                    <div className="border-grey border-r border-solid text-center w-1/2 pt-10 pr-10 pl-10">
+                    <div className={`${contentStyle} pt-8 sm:border-b-0 sm:border-r sm:pb-5`}>
                         {content[2]}
                     </div>
-                    <div className="text-center w-1/2 pt-10 pr-10 pl-10">
+                    <div className={`text-center w-full sm:w-1/2 sm:border-b-0 pt-8 pr-10 pl-10 pb-5`}>
                         {content[3]}
                     </div>
                 </div>

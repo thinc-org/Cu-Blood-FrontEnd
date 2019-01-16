0243a94a8993e83ada3322edc480f3d957692fd6
Boom PT <37561295+boompikachu@users.noreply.github.com>
2019-01-17 02:20:53 +0700

    Adjust Calendar cards

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ class App extends Component {
         <EventContainer />
         <Statistic />
         <FacebookContainer />
-        {/* <EventCard/> */}
         <Footer />
       </div>
     );

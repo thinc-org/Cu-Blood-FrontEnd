c965f85299b49743cd800783d19f8be0c0854f76
codefactor-io <support@codefactor.io>
2019-01-16 16:02:08 +0000

    [CodeFactor] Apply fixes

--- src/App.test.js
+++ src/App.test.js
@@ -2,36 +2,36 @@ import React from 'react'
 import { shallow } from 'enzyme'
 
 import App from './App'
+
+import Statistic from "./component/Statistic";
+
+import Navbar from './component/Navbar';
+
+import EventContainer from './component/EventContainer';
+
+import HomeHead from './component/HomeHead';
+
+import FacebookContainer from './component/FacebookContainer';
+
+import Footer from './component/Footer';
 it('renders without crashing', () => {
     shallow(<App />);
 });
-
-import Statistic from "./component/Statistic";
 it('renders without crashing', () => {
   shallow(<Statistic />);
 });
-
-import Navbar from './component/Navbar';
 it('renders without crashing', () => {
   shallow(<Navbar />);
 });
-
-import EventContainer from './component/EventContainer';
 it('renders without crashing', () => {
   shallow(<EventContainer/>);
 });
-
-import HomeHead from './component/HomeHead';
 it('renders without crashing', () => {
     shallow(<HomeHead/>);
 });
-
-import FacebookContainer from './component/FacebookContainer';
 it('renders without crashing', () => {
     shallow(<FacebookContainer/>);
 });
-
-import Footer from './component/Footer';
 it('renders without crashing', () => {
     shallow(<Footer/>);
 });

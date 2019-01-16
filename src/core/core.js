c078893fdee886bb9eda16fa7e3b5250141f344b
FiveChjr <f5.chjr@gmail.com>
2019-01-17 00:56:37 +0700

    (fix) axios interceptors

--- src/core/core.js
+++ src/core/core.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 
-axios.interceptors.request.use(function (config) {
+const instance = axios.create({    
+    baseURL: 'https://jsonplaceholder.typicode.com'
+});
+
+instance.interceptors.request.use(function (config) {
     config.headers.authorization = `Bearer ${localStorage.getItem('UserToken')}`
     return config
 }, function (error) {
     return Promise.reject(error)
 })
-
-const instance = axios.create({    
-    baseURL: 'https://jsonplaceholder.typicode.com'
-});
 
 export default instance; 

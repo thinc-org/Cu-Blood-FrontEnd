9f5fc7bbe74580fec69289a4f25fc875c371531a
Poom K <pattarapon.kitti@gmail.com>
2019-01-16 18:55:11 +0700

    Change media query for home header

--- src/component/HomeHead.js
+++ src/component/HomeHead.js
@@ -12,8 +12,8 @@ class HomeHead extends Component {
 
     return (
       <div className="flex justify-center bg-cb-grey-lighter font-cu-heading pt-10">
-        <div className="flex flex-col sm:flex-row sm:justify-center w-3/5">
-          <div className="w-full sm:w-1/2 my-10 text-center sm:text-left">
+        <div className="flex flex-col md:flex-row md:justify-center w-3/5">
+          <div className="w-full md:w-1/2 my-10 text-center md:text-left">
             <h1 className="text-cb-red mb-10" style={{fontSize: "4rem",}}>CU Blood</h1>
             <p className="text-3xl font-normal mb-6" style={font}>
               โครงการรณรงค์ให้นิสิตปัจจุบัน นิสิตเก่า คณาจารย์ และบุคลากร ของ
@@ -23,12 +23,12 @@ class HomeHead extends Component {
             <button className="bg-cb-red rounded-lg text-white px-12 pt-4 pb-2 mb-6">
               ลงทะบียน
             </button>
-            <div className="flex items-start justify-center sm:justify-start">
+            <div className="flex items-start justify-center md:justify-start">
               <p className="text-cb-red font-semibold mr-2">เกี่ยวกับโครงการ</p>
               <img src={Arrow} alt="arrow" className="w-6" />
             </div>
           </div>
-          <div className="h-64 sm:h-auto w-full sm:w-1/2 flex sm:flex-col md:flex-row justify-center md:justify-start items-center md:pl-10">
+          <div className="w-full md:w-1/2 flex md:flex-row justify-center md:justify-start items-center md:pl-10">
             <img
               src={Logo}
               alt="CU Blood logo"

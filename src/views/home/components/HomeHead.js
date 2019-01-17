643f05dbe35cbaa07b11c7b398528ef46661f4c1
Boom PT <37561295+boompikachu@users.noreply.github.com>
2019-01-17 23:50:44 +0700

    Fix HomeHead

--- src/views/home/components/HomeHead.js
+++ src/views/home/components/HomeHead.js
@@ -11,31 +11,39 @@ class HomeHead extends Component {
     };
 
     return (
-      <div className="flex justify-center bg-cb-grey-lighter font-cu-heading py-10">
-        <div className="flex flex-col md:flex-row md:justify-center w-4/5 sm:w-3/5">
-          <div className="w-full md:w-1/2 text-center md:text-left mb-6 md:mb-0">
-            <div className="text-6xl text-cb-red font-black mb-6">CU Blood</div>
+      <div id="homeHeadBanner" className="flex bg-cb-grey-lighter font-cu-heading py-10 overflow-hidden lg:flex-row  flex-col-reverse ">
+                          {/*<div className="flex flex-col md:flex-row md:justify-center w-4/5 sm:w-3/5">*/}
+        <div id="leftBoxHomeHeaderBanner" className="flex w-full lg:w-1/2 flex-col">
+          <div className="w-3/4 flex flex-col items-center self-center lg:items-start lg:self-end text-center lg:text-left">
+            <div className="text-6xl text-cb-red font-black mb-6 ">
+              CU Blood
+            </div>
             <div className="text-3xl font-normal mb-6" style={font}>
               โครงการรณรงค์ให้นิสิตปัจจุบัน นิสิตเก่า คณาจารย์ และบุคลากร ของ
               <span className="font-black">จุฬาลงกรณ์มหาวิทยาลัย</span>
               ร่วมกันบริจาคโลหิตให้แก่สภากาชาดไทย
             </div>
-            <div className="flex justify-center md:justify-start">
-              <button className="text-lg bg-cb-red rounded-lg text-white px-12 h-12 mb-6 leading-none">
-                ลงทะบียน
-              </button>              
-            </div>
-            <div className="flex items-center justify-center md:justify-start">
-              <p className="text-lg text-cb-red font-normal mr-2">
-                เกี่ยวกับโครงการ
-              </p>
-              <img src={Arrow} alt="arrow" className="w-6 pb-2" />
-            </div>
+          
+          <div className="flex justify-center md:justify-start">
+            <button className="text-lg bg-cb-red rounded-lg text-white px-12 h-12 mb-6 leading-none">
+              ลงทะบียน
+            </button>              
           </div>
-          <div className="w-full md:w-1/2 flex md:flex-row justify-center md:justify-start items-center md:pl-10">
-            <img src={Logo} alt="CU Blood logo" className="w-24 sm:w-32 mr-6" />
-            <img src={Cross} alt="Cross" className="w-6 mr-6" />
-            <img src={Blood} alt="Red Cross logo" className="w-32 sm:w-48" />
+          <div className="flex items-center justify-center md:justify-start">
+            <p className="text-lg text-cb-red font-normal mr-2">
+              เกี่ยวกับโครงการ
+            </p>
+            <img src={Arrow} alt="arrow" className="w-6 pb-2" />
+          </div>
+          </div>
+        </div>
+                            
+                          {/*<div className="w-full md:w-1/2 flex md:flex-row justify-center md:justify-start items-center md:pl-10">*/}
+        <div id="rightBoxHomeHeaderBanner" className="w-full lg:w-1/2 flex overflow-hidden flex-col justify-center py-8 lg:py-0">
+          <div className="flex flex-row lg:self-start self-center items-center ">
+            <img src={Logo} alt="CU Blood logo" className=" h-full px-6 pl-10" style={{width: '11rem'}} />
+            <img src={Cross} alt="Cross" className="w-6 h-full" />
+            <img src={Blood} alt="Red Cross logo" className=" h-full px-6" style={{width: '15rem'}}/>
           </div>
         </div>
       </div>

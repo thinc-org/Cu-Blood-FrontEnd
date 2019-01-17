193fcf27eaac3801f8f091bcf4b9528233d04ef6
new5558 <new17353@gmail.com>
2019-01-17 17:21:11 +0700

    add language support

--- src/views/shared-components/Navbar.js
+++ src/views/shared-components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import logo from '../../assets/logo/logo1.svg'
+import earth from '../../assets/logo/ios-globe.svg'
 
 export default () => {
     return (
-        <div className="font-cu-heading static md:sticky md:pin-t md:pin-l w-full shadow z-40">
+        <div className="font-cu-heading leading-none static md:sticky md:pin-t md:pin-l w-full shadow z-40">
             <div className="flex justify-center py-3 bg-white">
                 <div className="flex justify-between w-3/4" style={{ maxWidth: '1080px' }}>
                     <div className="flex items-center">
@@ -16,9 +17,15 @@ export default () => {
                             <li className="mx-3">ติดต่อเรา</li>
                         </ul>
                     </div>
-                    <ul className="list-reset py-6">
-                        <li className="text-red">เข้าสู่ระบบ</li>
-                    </ul>
+                    <div className="flex items-center">
+                        <ul className="list-reset py-6">
+                            <li className="text-cb-red hidden md:block">เข้าสู่ระบบ</li>
+                        </ul>
+                        <div className=" flex items-center rounded-lg ml-4 p-2 bg-cb-pink-light">
+                            <img className="h-6" src={earth} alt="change language" />
+                            <span className="ml-2 text-cb-red">THAI</span>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="border-t block md:hidden"></div>
--- src/views/shared-components/NavbarMobile.js
+++ src/views/shared-components/NavbarMobile.js
@@ -4,6 +4,7 @@ export default () => {
     return (
         <div className="justify-center sticky md:static pin-l pin-t py-3 bg-white flex md:hidden">
             <ul className="list-reset flex py-3 text-grey-darkest whitespace-no-wrap overflow-x-auto">
+                <li className="text-red">เข้าสู่ระบบ</li>
                 <li className="inline mx-3">เกี่ยวกับเรา</li>
                 <li className="mx-3">ข้อควรรู้</li>
                 <li className="mx-3">ข่าวประกาศ</li>

7700a5607c8f6dca5a4776c8dc4a6ba8f0d24def
Poom K <pattarapon.kitti@gmail.com>
2019-01-17 20:50:10 +0700

    Fix spacing of UrgentAnnouncement

--- src/views/home/components/UrgentAnnouncement.js
+++ src/views/home/components/UrgentAnnouncement.js
@@ -4,12 +4,12 @@ class UrgentAnnouncement extends Component {
   render() {
     return (
       <div className="w-full flex justify-center bg-white">
-        <div className="border-cb-grey-border border-b flex flex-col sm:flex-row w-4/5 sm:w-3/5 pb-6 sm:pb-8 mt-10 font-cu-heading">
-          <div className="border-cb-grey-border border-b sm:border-b-0 sm:border-r w-full sm:w-1/2 pb-6 sm:pb-0 sm:pr-8 mb-6 sm:mb-0 flex justify-start sm:justify-end items-end">
+        <div className="flex flex-col sm:flex-row w-full pb-6 sm:pb-8 mt-10 font-cu-heading justify-center">
+          <div className="border-cb-grey-border border-b sm:border-r pb-4 px-4 sm:pb-0 md:pr-8 mb-6 sm:mb-0 flex justify-start sm:justify-end">
             {this.contentDate(`18-21`, `มกราคม`)}
             {this.contentDesc(`วันบริจาคเลือด`, `อีก 3 วัน`)}
           </div>
-          <div className="w-full sm:w-1/2 sm:ml-8 flex items-end">
+          <div className="border-cb-grey-border border-b pb-4 md:pl-8 px-4 flex items-end">
             {this.contentDate(17, `มกราคม`)}
             {this.contentDesc(`ปิดการลงทะเบียน`, `อีก 2 วัน`)}
           </div>
@@ -21,7 +21,7 @@ class UrgentAnnouncement extends Component {
 
   contentDate = (date, month) => {
     return (
-        <div className="mr-10 text-right text-cb-red">
+        <div className="mr-4 text-right text-cb-red">
             <p className="text-3xl font-black">{date}</p>
             <p className="text-xl font-normal" style={{color: "#cf2333"}}>{month}</p>
         </div>
@@ -31,7 +31,7 @@ class UrgentAnnouncement extends Component {
   contentDesc = (title, time) => {
       return (
       <div className="flex flex-col">
-        <p className="text-3xl font-black break-words">{title}</p>
+        <p className="text-3xl font-black">{title}</p>
         <p className="text-xl font-normal" style={{color: "#4c4c4c"}}>{time}</p>
       </div>
       )

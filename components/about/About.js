da24d794b0629a1b19049d589cc5c35af11d2fe5
Boom PT <37561295+boompikachu@users.noreply.github.com>
2019-01-19 10:54:21 +0700

    Added Header

--- components/about/About.js
+++ components/about/About.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import AboutHeader from './local-components/AboutHeader';
+import PageHeader from '@/shared-components/PageHeader';
 
 class About extends React.Component {
     render() {
         return (
             <div>
                 <AboutHeader />
-
+                <PageHeader english="STAFFS" englishColor="text-pink" thai="บุคลากร" borderColor="border-red"/>
             </div>
         );
     }

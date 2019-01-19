6602cf47fdfdf30c189831fe85d917582a6a736d
new5558 <new17353@gmail.com>
2019-01-19 20:16:29 +0700

    apply fix

--- pages/about.js
+++ pages/about.js
@@ -8,14 +8,14 @@ class About extends React.Component {
     render() {
         return (
             <div>
-                    <AboutHeader />
-                    <PageHeader english="STAFFS" englishColor="text-pink" thai="บุคลากร" borderColor="border-red" />
-                    <div className="flex flex-col md:flex-row justify-center flex-wrap">
-                        <AboutCard englishName="Boom" myRole="Front-end" picture="/static/logo/ios-globe.svg" />
-                        <AboutCard englishName="New" myRole="Front-end" picture="/static/logo/ios-globe.svg" />
-                        <AboutCard englishName="Poom" myRole="Front-end" picture="/static/logo/ios-globe.svg" />
-                    </div>
+                <AboutHeader />
+                <PageHeader english="STAFFS" englishColor="text-pink" thai="บุคลากร" borderColor="border-red" />
+                <div className="flex flex-col md:flex-row justify-center flex-wrap">
+                    <AboutCard englishName="Boom" myRole="Front-end" picture="/static/logo/ios-globe.svg" />
+                    <AboutCard englishName="New" myRole="Front-end" picture="/static/logo/ios-globe.svg" />
+                    <AboutCard englishName="Poom" myRole="Front-end" picture="/static/logo/ios-globe.svg" />
                 </div>
+            </div>
         );
     }
 }

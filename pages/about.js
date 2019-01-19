350e781d694cb28b254bcbbcde1c7fa287dd7e7e
new5558 <new17353@gmail.com>
2019-01-19 13:07:20 +0700

    added uncomplete routing

---
+++ pages/about.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import AboutHeader from '@/about/local-components/AboutHeader';
+import PageHeader from '@/shared-components/PageHeader';
+import AboutCard from '@/about/local-components/AboutCard';
+import Main from '$/main'
+
+class About extends React.Component {
+    render() {
+        return (
+            <Main>
+                <div>
+                    <AboutHeader />
+                    <PageHeader english="STAFFS" englishColor="text-pink" thai="บุคลากร" borderColor="border-red" />
+                    <div className="flex flex-col md:flex-row justify-center flex-wrap">
+                        <AboutCard englishName="Boom" myRole="Front-end" picture="/static/logo/ios-globe.svg" />
+                        <AboutCard englishName="New" myRole="Front-end" picture="/static/logo/ios-globe.svg" />
+                        <AboutCard englishName="Poom" myRole="Front-end" picture="/static/logo/ios-globe.svg" />
+                    </div>
+                </div>
+            </Main>
+        );
+    }
+}
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
+import Statistic from "@/home/local-components/Statistic";
+// import EventContainer from './components/EventContainer';
+import HomeHead from '@/home/local-components/HomeHead';
+import FacebookContainer from '@/shared-components/FacebookContainer';
+import Footer from '@/shared-components/Footer';
+import Announcement from '@/home/local-components/Announcement';
+import UrgentAnnouncement from '@/home/local-components/UrgentAnnouncement';
 import Main from '$/main'
-// import Home from '@/home/Home'
-import About from '@/about/About'
-import '../static/css/index.css'
+
 
 class App extends Component {
+
   render() {
+
     return (
       <Main>
-        {/* <Home /> */}
-        <About />
+        <div className="font-sans border-black flex flex-col content-center w-screen" >
+          <HomeHead />
+          <UrgentAnnouncement />
+          <Announcement />
+          {/* <EventContainer /> */}
+          <Statistic />
+          <FacebookContainer />
+          <Footer />
+        </div>
       </Main>
     );
   }

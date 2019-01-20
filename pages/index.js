ecf551822ccdd4027521190cccbefe932fa35e9e
new5558 <new17353@gmail.com>
2019-01-20 12:41:08 +0700

    fix axios ssr

--- pages/index.js
+++ pages/index.js
@@ -6,23 +6,24 @@ import FacebookContainer from '@/shared-components/FacebookContainer';
 import Footer from '@/shared-components/Footer';
 import Announcement from '@/home/local-components/Announcement';
 import UrgentAnnouncement from '@/home/local-components/UrgentAnnouncement';
-import axios from '@/core/core';
+// import axios from '@/core/core';
 import '../static/css/index.css';
+import axios from '@/core/core';
 
 class App extends Component {
 
   static async getInitialProps() {
      //Save announcementData to state
-    const announcementData = await axios.get('./posts')
+    const announcementData = await axios.get('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.data.length)
-      .then(dataNumber => axios.get(`./posts?_start=${dataNumber - 5}&_limit=5`))
+      .then(dataNumber => axios.get(`https://jsonplaceholder.typicode.com/posts?_start=${dataNumber - 5}&_limit=5`))
       .then(response => response.data)
       .catch(console.log);
 
     //Save statData to state
-    const statData = await axios.get('./posts')
+    const statData = await axios.get('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.data.length)
-      .then(dataNumber => axios.get(`./posts?_start=${dataNumber - 4}&_limit=4`))
+      .then(dataNumber => axios.get(`https://jsonplaceholder.typicode.com/posts?_start=${dataNumber - 4}&_limit=4`))
       .then(response => response.data)
       .catch(console.log)
 

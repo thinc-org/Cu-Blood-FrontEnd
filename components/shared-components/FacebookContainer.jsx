539936e767097e7b2dddbdb91548f198bcc187c8
new5558 <new17353@gmail.com>
2019-01-21 14:30:27 +0700

    apply heavy fix on facebook + day of posts

--- components/shared-components/FacebookContainer.jsx
+++ components/shared-components/FacebookContainer.jsx
@@ -7,22 +7,21 @@ import map from 'lodash/map';
 class FacebookZone extends React.Component {
 
     render() {
-        const {posts} = this.props;
-        const title = map(posts, 'title');
-        const body = map(posts, 'message');
-        const pictureUrl = map(posts, 'full_picture');
-        const url = map(posts, 'permalink_url')
+        const { posts } = this.props;
+        // const body = map(posts, 'message');
+        // const pictureUrl = map(posts, 'full_picture');
+        // const url = map(posts, 'permalink_url')
 
         // layout-wide
         return (
-            <div id="root" className="flex flex-col items-center text-white" style={{ backgroundColor: '#8e9dc0' }}>
-                <PageHeader borderColor="border-white" thaiColor="white" thai="CU Blood on Facebook" englishColor="text-grey-light text-xs" english="OUR ACTIVITY" />
-                <div className="flex flex-col items-center lg:flex-row w-screen layout-wide" >
-                    <FacebookCard title={title[0]} body={body[0]} pictureUrl={pictureUrl[0]} url={url[0]}/>
-                    <FacebookCard title={title[1]} body={body[1]} pictureUrl={pictureUrl[1]} url={url[1]}/>
-                    <FacebookCard title={title[2]} body={body[2]} pictureUrl={pictureUrl[2]} url={url[2]}/>
-                </div>
-                <div className="mb-10">
+            <div className="" style={{ backgroundColor: '#8e9dc0' }}>
+                <div className="flex flex-col items-center text-white layout-wide pb-10">
+                    <PageHeader borderColor="border-white" thaiColor="white" thai="CU Blood on Facebook" englishColor="text-grey-light text-xs" english="OUR ACTIVITY" />
+                    <div className="flex flex-col justify-between items-center md:items-stretch lg:flex-row w-full md:mb-10" >
+                        <FacebookCard post={posts[0]} />
+                        <FacebookCard post={posts[1]} />
+                        <FacebookCard post={posts[2]} />
+                    </div>
                     <FacebookButton />
                 </div>
             </div>
--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ import I18 from '@/core/i18n';
 class App extends Component {
 
   static async getInitialProps() {
-     //Save announcementData to state
+    //Save announcementData to state
     const announcementData = await axios.get('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.data.length)
       .then(dataNumber => axios.get(`https://jsonplaceholder.typicode.com/posts?_start=${dataNumber - 5}&_limit=5`))
@@ -27,7 +27,7 @@ class App extends Component {
       .then(response => response.data)
       .catch(console.log);
 
-    const facebookPosts = await axios.get('https://api-dev.fives.cloud/api/v1//public/facebook')
+    const facebookPosts = await axios.get('https://api-dev.fives.cloud/api/v1/public/facebook')
       .then(response => response.data)
       .then(result => result.data)
       .catch(console.log);
@@ -40,7 +40,7 @@ class App extends Component {
   }
 
   render() {
-    const {announcementData, statData, facebookPosts} = this.props;
+    const { announcementData, statData, facebookPosts } = this.props;
     console.log(facebookPosts, 'posts')
     //If data is not fetched
     if (!(announcementData && statData)) {
@@ -51,13 +51,13 @@ class App extends Component {
 
     //If data is fetched
     return (
-      <div className="font-sans border-black flex flex-col content-center w-screen" >
+      <div className="font-sans border-black flex flex-col content-center w-full" >
         <HomeHead />
         <UrgentAnnouncement />
         <Announcement announcementData={announcementData} />
         {/* <EventContainer /> */}
         <Statistic statData={statData} />
-        <FacebookContainer posts={facebookPosts}/>
+        <FacebookContainer posts={facebookPosts} />
         <Footer />
       </div>
     );

3dcdf9945d9c80b4c268f12661f0c448530ade38
new5558 <new17353@gmail.com>
2019-01-21 22:49:08 +0700

    fix layout

--- components/home/local-components/UrgentAnnouncement.jsx
+++ components/home/local-components/UrgentAnnouncement.jsx
@@ -9,13 +9,13 @@ class UrgentAnnouncement extends Component {
     return (
       <div className="bg-cb-red">
         <div className="layout-wide flex justify-center text-white">
-          <div className="flex flex-col sm:flex-row w-full mt-10 font-cu-heading justify-center">
-            <div className="pb-4 sm:pb-0 sm:pr-4 md:pr-8 mb-6 sm:mb-0 flex justify-start sm:justify-end">
+          <div className="flex flex-col sm:flex-row py-6 w-full items-center font-cu-heading justify-center">
+            <div className=" sm:pb-0 sm:pr-4 md:pr-8 mb-6 sm:mb-0 flex justify-start sm:justify-end">
               {this.contentDate(`18-21`, `มกราคม`)}
               {this.contentDesc(`วันบริจาคเลือด`, `อีก 3 วัน`)}
             </div>
             <div className="border-l h-16" style={line}></div>
-            <div className="pb-4 sm:pl-4 md:pl-8 flex items-end">
+            <div className=" sm:pl-4 md:pl-8 flex items-end">
               {this.contentDate(17, `มกราคม`)}
               {this.contentDesc(`ปิดการลงทะเบียน`, `อีก 2 วัน`)}
             </div>
@@ -28,8 +28,8 @@ class UrgentAnnouncement extends Component {
   contentDate = (date, month) => {
     return (
         <div className="mr-4 text-right">
-            <p className="text-3xl">{date}</p>
-            <p className="text-xl font-normal" >{month}</p>
+            <p className="text-3xl font-bold">{date}</p>
+            <p className="text-xl font-normal leading-none" >{month}</p>
         </div>
     );
   }
@@ -38,7 +38,7 @@ class UrgentAnnouncement extends Component {
       return (
       <div className="flex flex-col">
         <p className="text-3xl">{title}</p>
-        <p className="text-xl font-normal" style={{color:'rgba(255, 255, 255, 0.5)'}}>{time}</p>
+        <p className="text-xl font-normal leading-none" style={{color:'rgba(255, 255, 255, 0.5)'}}>{time}</p>
       </div>
       )
   }

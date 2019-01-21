d6832555ff8b570ae3ce14c1f1c1d8453a1264a7
new5558 <new17353@gmail.com>
2019-01-21 18:54:30 +0700

    fix padding of facebook btn

--- components/shared-components/FacebookButton.jsx
+++ components/shared-components/FacebookButton.jsx
@@ -4,7 +4,7 @@ class FacebookButton extends React.Component {
 
     render() {
         return(
-            <div className="flex items-center rounded-lg py-2 px-6 font-cu-heading" style={{backgroundColor: "#7685a6"}}>
+            <div className="flex items-center rounded-lg px-6 py-2 font-cu-heading" style={{backgroundColor: "#7685a6"}}>
                 <img className="" src="/static/logo/flogo.svg" style={{
                     width: 30,
                     marginRight: 10

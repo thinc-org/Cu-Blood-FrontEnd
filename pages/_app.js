0ceb6b1d9bdf144884e2bd8ca60cadb6d7467dce
new5558 <new17353@gmail.com>
2019-01-24 16:30:34 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -3,19 +3,54 @@ import Main from '$/main'
 import App, { Container } from 'next/app'
 import NProgress from "next-nprogress/component"
 import NextI18Next from '@/core/i18n'
+import redirectTo from '@/core/redirectTo.js'
+import cookies from 'next-cookies'
+import axios from '@/core/core'
 
 class MyApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
     let pageProps = {}
+    const c = cookies(ctx);
 
     if (Component.getInitialProps) {
       pageProps = await Component.getInitialProps(ctx)
     }
+    // console.log(ctx.pathname ,ctx.pathname.includes('/u/'), 'pathname')
+    // if is in user page
+    if (ctx.pathname.includes('/u/') || true) {
+      console.log('true?')
+      if (typeof c.accessToken === 'undefined' && false) redirectTo('/', { res: ctx.res, status: 301 })
+      //if on another page return to login
+      else {
+        var response = await axios.post('https://api-dev.fives.cloud/api/v1/private/profile/info', {
+          // accessToken: cookies(ctx).accessToken
+          accessToken: 1
+        })
+          .then(resp => {
+            if (ctx.pathname == "/register") {
+              console.log('register')
+              // if(document) document.cookie = "accessToken=1; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+                redirectTo('/about', { res: ctx.res, status: 301 });
+            }
 
-    return { pageProps }
+            else {
+              //if auth check was successful, stay where we are
+              return { ...pageProps, ...{ query: ctx.query, authtoken: c.authtoken } };
+            }
+
+          })
+          .catch((err) => {
+            // if(document) document.cookie = "accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+            return { pageProps }; 
+          })
+      }
+    }
+
+    if (response !== null) { return { response }; }
+    else return { pageProps };
   }
 
-  render () {
+  render() {
     const { Component, pageProps } = this.props
 
     return (
@@ -25,7 +60,7 @@ class MyApp extends App {
           spinner={false}
         />
         <Main {...pageProps}>
-         <Component {...pageProps} />
+          <Component {...pageProps} />
         </Main>
       </Container>
     )
---
+++ pages/u/test.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import FacebookContainer from '@/shared-components/FacebookContainer';
+import axios from '@/core/core';
+import Footer from '@/shared-components/Footer';
+import ContactHeader from '@/contact/local-components/ContactHeader';
+import ContactContent from '@/contact/local-components/ContactContent';
+import I18 from '@/core/i18n';
+
+class Test extends Component {
+    static async getInitialProps() {
+        const facebookPosts = await axios.get('https://api-dev.fives.cloud/api/v1/public/facebook')
+        .then(response => response.data)
+        .then(result => result.data)
+        .catch(console.log);
+
+        return {facebookPosts};
+    }
+    
+    render() {
+        console.log('test rendered')
+        const {facebookPosts} = this.props
+        return (
+            <div>
+                <ContactHeader />
+                <ContactContent />
+                <FacebookContainer posts={facebookPosts} />
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default Test;

c9bbffd6dca47e85310c70d4d1e93649e8cbfc6b
new5558 <new17353@gmail.com>
2019-01-25 14:42:08 +0700

    minify css size bt using purge css

--- next.config.js
+++ next.config.js
@@ -1,2 +1,20 @@
+class TailwindExtractor {
+    static extract(content) {
+        return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+    }
+}
+
 const withCSS = require('@zeit/next-css')
-module.exports = withCSS({})
+const withPurgeCss = require('next-purgecss')
+module.exports = withCSS(withPurgeCss(
+    {
+    purgeCss: {
+        extractors: [
+            {
+                extractor: TailwindExtractor,
+                extensions: ['js', 'jsx', 'ts', 'tsx', 'svg', 'html'],
+            }
+        ]
+    }
+}
+))

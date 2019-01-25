8d01598f7a037c122dc261b96de4df9302e0d74a
Boom PT <37561295+boompikachu@users.noreply.github.com>
2019-01-25 22:58:14 +0700

    Finish Major Card
    No Axios yet

--- components/home/local-components/CalendarContainer.jsx
+++ components/home/local-components/CalendarContainer.jsx
@@ -9,16 +9,12 @@ export default class CalendarContainer extends React.Component {
         const eventTitle = map(calendarEvents, 'title');
         return (
             <div className="bg-cb-pink-light">
-                <div>
-                    <PageHeader borderColor="border-cb-red" english="Calendar" thai="ปฏิทินกิจกรรม" englishColor="text-cb-pink" />
-                    <MinorEvent title={eventTitle[234324]} />
-                    {/* <LineBreak /> */}
-                    <MinorEvent title={eventTitle[1]} />
-                    {/* <LineBreak /> */}
-                    <MinorEvent title={eventTitle[23324]} />
-                    {/* <LineBreak /> */}
-                    <MinorEvent title={eventTitle[3]} hasLineBreak={false}/>
-                </div>
+                <PageHeader borderColor="border-cb-red" english="Calendar" thai="ปฏิทินกิจกรรม" englishColor="text-cb-pink" />
+                <MajorEvent />
+                <MinorEvent title={eventTitle[1]} />
+                <MinorEvent title={eventTitle[2]} />
+                <MinorEvent title={eventTitle[3]} />
+                <MinorEvent title={eventTitle[4]} hasLineBreak={false} />
             </div>
         );
     }
@@ -52,10 +48,48 @@ const MinorEvent = (props) => {
     }
 }
 
-const LineBreak = ({hasLineBreak = true}) => {
+
+
+const MajorEvent = (props) => {
+    return (
+        <div className="layout-wide">
+            <div className="bg-white layout-narrow rounded-lg font-cu-heading text-3xl py-8 shadow-md " style={{ maxWidth: '800px', paddingLeft: '0', paddingRight: '0', }}>
+                <div className="flex flex-row">
+                    <div className="flex flex-col text-pink-dark px-8 flex-start">
+                        <div className="font-bold">
+                            27
+                    </div>
+                        <div className="font-medium">
+                            January
+                    </div>
+                    </div>
+                    <div className="flex flex-col pr-8 text-wrap">
+
+                        ประกาศเปิดตัวเว็บไซต์ CU Blood โดย Thinc. Development
+    
+                    <div className="flex flex-row text-base pt-8">
+                            <div className="w-1/2 text-pink-dark flex flex-row items-center ">
+                                <img className="w-14 h-14 mr-4" src='/static/logo/greyClock.png' alt="clock"></img>
+                                อาคาร 100 ปี คณะวิศวกรรมศาสตร์
+                        </div>
+                            <div className="w-1/2 text-grey-darker flex flex-row items-center">
+                                <img className="w-14 h-14 mr-4" src='/static/logo/location.png' alt="clock"></img>
+                                10.00 AM - 12.00 PM
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+
+const LineBreak = ({ hasLineBreak = true }) => {
     return hasLineBreak ?
         (
-            <hr className="border-solid border-grey layout-wide" style={{ borderBottomWidth: '0.05rem' }} />
+            <hr className="border-solid border-grey layout-wide" style={{ borderBottomWidth: '0.05rem', maxWidth: '600px' }} />
         )
         :
         (

58e7c75a29a1730cfe0fa39724d0eba806fbcc3e
new5558 <new17353@gmail.com>
2019-01-26 22:33:59 +0700

    introducting new component : topic

--- components/home/local-components/Announcement.jsx
+++ components/home/local-components/Announcement.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import AnnouncementContent from './AnnouncementContent';
 import Header from "@/shared-components/PageHeader";
+import Topic from "@/shared-components/Topic";
 
 class Announcement extends Component {
 
@@ -10,7 +11,7 @@ class Announcement extends Component {
                 <div className="bg-cover bg-center hidden md:block w-screen max-w-xs xl:max-w-md" style={{ backgroundImage: 'url(../../../static/home/announcementBackground.jpg)' }}></div>
                 <div className="font-cu-heading flex flex-col md:px-10">
                     <div className="hidden md:block">
-                        <SpecialHeader borderColor="border-cb-red" english="ANNOUNCEMENT" thai="ข่าวประกาศ" englishColor="text-cb-pink" />
+                        <Topic borderColor="border-cb-red" english="ANNOUNCEMENT" thai="ข่าวประกาศ" englishColor="text-cb-pink" />
                     </div>
                     <div className="block md:hidden">
                         <Header borderColor="border-cb-red" english="ANNOUNCEMENT" thai="ข่าวประกาศ" englishColor="text-cb-pink" />
@@ -20,18 +21,6 @@ class Announcement extends Component {
             </div>
         );
     }
-}
-
-// just temporary patch for prototype
-// will revise PageHeader.jsx to be able to appect those properties in spare time
-const SpecialHeader = ({ borderColor, english, thai, englishColor, thaiColor = 'black' }) => {
-    return (
-        <div className="font-cu-heading py-10 h-auto">
-            <div className={`text-base tracking-wide font-normal ${englishColor}`}>{english}</div>
-            <div className={`text-3xl font-black ${thaiColor}`}>{thai}</div>
-            <hr className={`w-24 mx-0 mt-2 ${borderColor}`} style={{ borderBottomWidth: "6px" }} />
-        </div>
-    );
 }
 
 export default Announcement;

9a1f54fa95b241a4a23c35000f7fafa18bcb117a
new5558 <new17353@gmail.com>
2019-01-29 02:44:34 +0700

    and console log to debug on dev deploy 2

--- components/core/UserInfoProvider.js
+++ components/core/UserInfoProvider.js
@@ -23,7 +23,7 @@ class UserInfoProvider extends Component {
         axios.post('https://api-dev.fives.cloud/v0/profile/logout')
             .then(() => this.setState({ userInfo: {}, isLogin: false }))
             .catch(console.log)
-            .finally(() => {noRedirect === true ? null : window.location.href = '/'})
+            // .finally(() => {noRedirect === true ? null : window.location.href = '/'})
         // })
     }
 

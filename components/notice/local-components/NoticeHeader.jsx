e2f086551660f42373172da528c88b911aff3673
Boom PT <37561295+boompikachu@users.noreply.github.com>
2019-01-29 14:33:05 +0700

    Finish Notice Header

--- components/notice/local-components/NoticeHeader.jsx
+++ components/notice/local-components/NoticeHeader.jsx
@@ -1,18 +1,47 @@
 import React from 'react'
 
 export default class NoticeHeader extends React.Component {
-    render () {
-        return(
+    render() {
+        return (
             <div className="font-cu-heading bg-cb-pink-light">
                 <div className="layout-wide py-20 text-center md:text-left" >
-                    <div className="text-6xl font-bold mb-6 text-black">
-                    ข้อควรรู้
+                    <div className="text-6xl font-bold mb-8 text-black">
+                        ข้อควรรู้
                     </div>
-                    <div className="text-3xl text-grey-darkest" >
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi veniam nihil ex sunt quasi possimus, esse voluptatem earum consequatur, iure aut eligendi impedit sint officia incidunt omnis. Facere, quasi excepturi.  
-                    </div>
+                    <Subheader thai={"คุณสมบัติผู้บริจาคโลหิต"} english={"CHARACTERISTIC AND CRITERIA"} />
+                    <Subheader thai={"ขั้นตอนการบริจาคโลหิตกับโครงการ"} english={"PROCEDURES"} />
+                    <Subheader thai={"การเตรียมตัวก่อน-หลังบริจาคโลหิต"} english={"PRE-POST DONATION PREPARATION"} hasLineBreak={false} />
                 </div>
             </div>
-        )
+        );
     }
+}
+
+
+const Subheader = (props) => {
+    const { thai, english, hasLineBreak = true } = props
+    return (
+        <div className="text-2xl ">
+            <div className="">
+                {thai}
+            </div>
+            <div className="text-pink-dark">
+                {english}
+            </div>
+            <div className={`py-6 ${hasLineBreak ? `` : `hidden`}`} style={{width: '25rem'}}>
+                <LineBreak hasLineBreak={hasLineBreak}/>
+            </div>
+        </div>
+    )
+}
+
+const LineBreak = ({ hasLineBreak = true }) => {
+    return hasLineBreak ?
+        (
+            <hr className="border-solid border-grey w-full " style={{ borderBottomWidth: '0.05rem'}} />
+        )
+        :
+        (
+            null
+        )
 }

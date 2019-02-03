aaf1d4cade523763f3b1f55c4564921644fa8b7a
new5558 <new17353@gmail.com>
2019-02-03 17:04:12 +0700

    complete form validation

--- components/register/local-components/RegisterFillForm.jsx
+++ components/register/local-components/RegisterFillForm.jsx
@@ -8,135 +8,166 @@ class RegisterFillForm extends Component {
             formValid: false,
             phoneNumber: "",
             phoneNumberValid: false,
-            formErrors: {phoneNumber: ""},
+            birthday: "",
+            birthdayValid: false,
+            weight: 0,
+            weightValid: false,
+            email: "",
+            emailValid: false,
+            formErrors: { phoneNumber: "", birthday: "", weight: "", email: "" },
         };
     }
 
     handleChange = (e) => {
         const name = e.target.name;
         const value = e.target.value;
-        this.setState({[name]: value}, 
+        this.setState({ [name]: value },
             () => this.validate(name, value))
-    } 
+    }
 
     validate = (name, value) => {
         let isValid = this.state[name]
         let formErrors = Object.assign({}, this.state.formErrors);
-        switch(name) {
+        switch (name) {
             case "phoneNumber":
                 isValid = (/^0[0-9]{9}$/i).test(value) ? true : false;
                 formErrors.phoneNumber = isValid ? "" : "เลขโทรศัพท์ไม่ถูกต้อง";
+                break;
+            case "birthday":
+                const endDate = this.props;
+                const end =  new Date(endDate)
+                const endAge = new Date(end.getFullYear(), end.getMonth(), end.getDate() + 3).getTime()
+                const nowAge = Date.now()
+                // console.log(end, 'end', nowAge, 'now', endAge - nowAge)
+                const ageDifMs = ((endAge - nowAge > 0) ? endAge : nowAge) - new Date(value).getTime();
+                // console.log(ageDifMs, 'ageDifMs', new Date(end.getFullYear(), end.getMonth(), end.getDate() + 3).getTime(), (new Date(value)).getTime())
+                const ageDate = new Date(ageDifMs); // miliseconds from epoch
+                isValid = 17 <= Math.abs(ageDate.getUTCFullYear() - 1970);
+                // console.log(isValid, Math.abs(ageDate.getUTCFullYear() - 1970));
+                formErrors.birthday = isValid ? "" : "คุณต้องมีอายุมากกว่า 17 ปี ในวันที่บริจาคเลือด";
+                break;
+            case "weight":
+                isValid = 20 < value;
+                formErrors.weight = isValid ? "" : "กรุณากรอกน้ำหนักให้ถูกต้อง";
+                break;
+            case "email":
+            isValid = 20 < value;
+                const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                isValid = re.test(value.toLowerCase());
+                formErrors.email = isValid ? "" : "อีเมลล์ของคุณไม่ถูกต้อง";
                 break;
             default:
                 return;
         }
         // console.log(formErrors)
-        this.setState({[name + "Valid"]: isValid, "formErrors": formErrors}, () => this.validateForm())
+        this.setState({ [name + "Valid"]: isValid, "formErrors": formErrors }, () => this.validateForm())
     }
 
-    validateForm = () => {
-        let isValid = true;
-        for(const key in this.state) {
-            if(key.toString().includes('Valid') && key.toString() !== 'formValid' && this.state[key] === false) {
-                isValid = false;
-                break;
-            }
+
+validateForm = () => {
+    let isValid = true;
+    for (const key in this.state) {
+        if (key.toString().includes('Valid') && key.toString() !== 'formValid' && this.state[key] === false) {
+            isValid = false;
+            break;
         }
-        this.setState({formValid : isValid}, () => console.log(this.state))
     }
+    this.setState({ formValid: isValid }, () => console.log(this.state))
+}
 
-    render() {
-        const inputClassName = `bg-cb-grey-light rounded-lg mt-2 p-2`;
-        return (
-            <form className="layout-wide flex flex-col items-center justify-center pb-10 sm:py-10">
-                <div>
-                    <FormGroup text="ข้อมูลติดต่อ">
-                        <Form text="อีเมลล์" width="full" smWidth="48">
-                            <Input type="email" />
-                        </Form>
-                        <Form text="เบอร์โทรศัพท์" width="full" smWidth="48">
-                            <Input value={this.state.phone} onChange={this.handleChange} type="text" name="phoneNumber" error={this.state.formErrors.phoneNumber} />
-                        </Form>
-                        <Form text="ที่อยู่" width="full">
-                            <textarea required className={`${inputClassName} w-full h-16`} />
-                        </Form>
-                    </FormGroup>
-                    <FormGroup text="ข้อมูลทั่วไป">
-                        <Form text="ชื่อ" width="full" smWidth="48">
-                            <Input type="text" />
-                        </Form>
-                        <Form text="นามสกุล" width="full" smWidth="48">
-                            <Input type="text" />
-                        </Form>
-                        <Form text="ชื่อเล่น" width="full" smWidth="48">
-                            <Input type="text" />
-                        </Form>
-                        <Form text="วัน/เดือน/ปี เกิด" width="full" smWidth="48">
-                            <Input type="date" />
-                        </Form>
-                        <Form text="เพศ" width="24">
-                            <Selector choices={['ชาย', 'หญิง']} />
-                        </Form>
-                        <Form text="ไซส์เสื้อ" width="24">
-                            <Selector choices={['M (38")', 'L (40")', 'XL (42")', 'XXL (44")']} />
-                        </Form>
-                        <Form text="น้ำหนัก" width="24">
-                            <Input type="number" />
-                        </Form>
-                        <Form text="สถานภาพ" width="24">
-                            <Selector choices={['นิสิตจุฬา', 'นิสิตเก่า', 'บุคลากร', 'อาจารย์']} />
-                        </Form>
-                        <Form text="สัญชาติ" width="24">
-                            <Selector choices={['ไทย', 'ต่างชาติ']} />
-                        </Form>
-                        <Form text="ชั้นปี" width="24">
-                            <Selector choices={['1', '2', '3', '4', '5', '6', "ปริญญาโท", 'ปริญญาเอก', 'อื่นๆ']} />
-                        </Form>
-                        <Form text="รหัสนิสิต" width="full" smWidth="48">
-                            <Input type="text" />
-                        </Form>
-                        <Form text="คณะ" width="full" smWidth="48">
-                            <Selector choices={
-                                ['คณะวิศวกรรมศาสตร์', 'คณะพาณิชยศาสตร์และการบัญชี', 'คณะวิทยาศาสตร์', 'คณะครุศาสตร์',
-                                    'คณะสหเวชศาสตร์ ', 'คณะอักษรศาสตร์', "คณะเภสัชศาสตร์", 'คณะเศรษฐศาสตร์', 'คณะทันตแพทยศาสตร์',
-                                    'คณะรัฐศาสตร์', 'คณะนิเทศศาสตร์', 'คณะจิตวิทยา', 'คณะนิติศาสตร์', 'คณะพยาบาลศาสตร์',
-                                    'คณะแพทยศาสตร์', 'คณะศิลปกรรมศาสตร์', 'คณะสถาปัตยกรรมศาสตร์', 'คณะสัตวแพทยศาสตร์',
-                                    'คณะวิทยาศาสตร์การกีฬา', 'วิทยาลัยวิทยาศาสตร์สาธารณสุข', 'บัณฑิตวิทยาลัย', 'สำนักวิชาทรัพยากรการเกษตร', 'อื่นๆ']
-                            } />
-                        </Form>
-                    </FormGroup>
-                    <FormGroup text="ข้อมูลทางการแพทย์">
-                        <Form text="โรคประจำตัว" width="full">
-                            <textarea className={`${inputClassName} w-full h-16`} />
-                        </Form>
-                        <Form text="หมู่เลือด" width="32" smWidth="48">
-                            <Selector choices={['A', 'B', 'O', 'AB']} />
-                        </Form>
-                        <Form text="RH" width="32" smWidth="48">
-                            <Selector choices={['+', '-']} />
-                        </Form>
-                        <div className="check">
-                            <label className="flex font-cu-heading text-normal cursor-pointer check-box">
-                                <input type="checkbox" />
-                                <div className="check-text flex">ท่านเคยบริจาคโลหิตมาก่อนหรือไม่</div>
-                            </label>
-                            <label className="flex font-cu-heading text-normal cursor-pointer check-box">
-                                <input type="checkbox" />
-                                <div className="check-text flex"><span>ท่านเคยเข้าร่วมบริจาคโลหิตกับโครงการ <br/><span className="text-cb-red font-semibold">CU BLOOD</span> มาก่อนหรือไม่</span></div>
-                            </label>
-                        </div>
-                    </FormGroup>
-                    <div className="flex flex-col items-center justify-center mt-6 md:mt-12">
+render() {
+    const inputClassName = `bg-cb-grey-light rounded-lg mt-2 p-2`;
+    const {endDate} = this.props;
+    return (
+        <form className="layout-wide flex flex-col items-center justify-center pb-10 sm:py-10">
+            <div>
+                <FormGroup text="ข้อมูลติดต่อ">
+                    <Form text="อีเมลล์" width="full" smWidth="48">
+                        <Input value={this.state.email} onChange={this.handleChange} type="email" name="email" error={this.state.formErrors.email} />
+                    </Form>
+                    <Form text="เบอร์โทรศัพท์" width="full" smWidth="48">
+                        <Input value={this.state.phone} onChange={this.handleChange} type="text" name="phoneNumber" error={this.state.formErrors.phoneNumber} />
+                    </Form>
+                    <Form text="ที่อยู่" width="full">
+                        <textarea required className={`${inputClassName} w-full h-16`} />
+                    </Form>
+                </FormGroup>
+                <FormGroup text="ข้อมูลทั่วไป">
+                    <Form text="ชื่อ" width="full" smWidth="48">
+                        <Input type="text" />
+                    </Form>
+                    <Form text="นามสกุล" width="full" smWidth="48">
+                        <Input type="text" />
+                    </Form>
+                    <Form text="ชื่อเล่น" width="full" smWidth="48">
+                        <Input type="text" />
+                    </Form>
+                    <Form text="วัน/เดือน/ปี เกิด" width="full" smWidth="48">
+                        <Input value={this.state.birthday} onChange={this.handleChange} type="date" name="birthday" error={this.state.formErrors.birthday} />
+                    </Form>
+                    <Form text="เพศ" width="24">
+                        <Selector choices={['ชาย', 'หญิง']} />
+                    </Form>
+                    <Form text="ไซส์เสื้อ" width="24">
+                        <Selector choices={['M (38")', 'L (40")', 'XL (42")', 'XXL (44")']} />
+                    </Form>
+                    <Form text="น้ำหนัก" width="24">
+                        <Input value={this.state.weight} onChange={this.handleChange} type="number" name="weight" error={this.state.formErrors.weight} />
+                    </Form>
+                    <Form text="สถานภาพ" width="24">
+                        <Selector choices={['นิสิตจุฬา', 'นิสิตเก่า', 'บุคลากร', 'อาจารย์']} />
+                    </Form>
+                    <Form text="สัญชาติ" width="24">
+                        <Selector choices={['ไทย', 'ต่างชาติ']} />
+                    </Form>
+                    <Form text="ชั้นปี" width="24">
+                        <Selector choices={['1', '2', '3', '4', '5', '6', "ปริญญาโท", 'ปริญญาเอก', 'อื่นๆ']} />
+                    </Form>
+                    <Form text="รหัสนิสิต" width="full" smWidth="48">
+                        <Input type="text" />
+                    </Form>
+                    <Form text="คณะ" width="full" smWidth="48">
+                        <Selector choices={
+                            ['คณะวิศวกรรมศาสตร์', 'คณะพาณิชยศาสตร์และการบัญชี', 'คณะวิทยาศาสตร์', 'คณะครุศาสตร์',
+                                'คณะสหเวชศาสตร์ ', 'คณะอักษรศาสตร์', "คณะเภสัชศาสตร์", 'คณะเศรษฐศาสตร์', 'คณะทันตแพทยศาสตร์',
+                                'คณะรัฐศาสตร์', 'คณะนิเทศศาสตร์', 'คณะจิตวิทยา', 'คณะนิติศาสตร์', 'คณะพยาบาลศาสตร์',
+                                'คณะแพทยศาสตร์', 'คณะศิลปกรรมศาสตร์', 'คณะสถาปัตยกรรมศาสตร์', 'คณะสัตวแพทยศาสตร์',
+                                'คณะวิทยาศาสตร์การกีฬา', 'วิทยาลัยวิทยาศาสตร์สาธารณสุข', 'บัณฑิตวิทยาลัย', 'สำนักวิชาทรัพยากรการเกษตร', 'อื่นๆ']
+                        } />
+                    </Form>
+                </FormGroup>
+                <FormGroup text="ข้อมูลทางการแพทย์">
+                    <Form text="โรคประจำตัว" width="full">
+                        <textarea className={`${inputClassName} w-full h-16`} />
+                    </Form>
+                    <Form text="หมู่เลือด" width="32" smWidth="48">
+                        <Selector choices={['A', 'B', 'O', 'AB']} />
+                    </Form>
+                    <Form text="RH" width="32" smWidth="48">
+                        <Selector choices={['+', '-']} />
+                    </Form>
+                    <div className="check">
                         <label className="flex font-cu-heading text-normal cursor-pointer check-box">
-                            <input required type="checkbox" />
-                            <div className="check-text flex"><span>I have <a href="https://google.com" target="_blank" rel="noopener noreferrer" className="no-underline"><span className="text-cb-pink font-semibold">read the guide</span></a> and agreed to <span className="text-cb-pink font-semibold">Terms and Conditions</span>.</span></div>
+                            <input type="checkbox" />
+                            <div className="check-text flex">ท่านเคยบริจาคโลหิตมาก่อนหรือไม่</div>
                         </label>
-                        <button disabled={!this.state.formValid} className="px-10 py-3 text-white bg-cb-red rounded-lg mt-6 btn" type="submit" id="confirm" >ลงทะเบียน</button>
+                        <label className="flex font-cu-heading text-normal cursor-pointer check-box">
+                            <input type="checkbox" />
+                            <div className="check-text flex"><span>ท่านเคยเข้าร่วมบริจาคโลหิตกับโครงการ <br /><span className="text-cb-red font-semibold">CU BLOOD</span> มาก่อนหรือไม่</span></div>
+                        </label>
                     </div>
+                </FormGroup>
+                <div className="flex flex-col items-center justify-center mt-6 md:mt-12">
+                    <label className="flex font-cu-heading text-normal cursor-pointer check-box">
+                        <input required type="checkbox" />
+                        <div className="check-text flex"><span>I have <a href="https://google.com" target="_blank" rel="noopener noreferrer" className="no-underline"><span className="text-cb-pink font-semibold">read the guide</span></a> and agreed to <span className="text-cb-pink font-semibold">Terms and Conditions</span>.</span></div>
+                    </label>
+                    <button disabled={!this.state.formValid} className="px-10 py-3 text-white bg-cb-red rounded-lg mt-6 btn" type="submit" id="confirm" >ลงทะเบียน</button>
                 </div>
-            </form>
-        );
-    }
+            </div>
+        </form>
+    );
+}
 }
 
 export default RegisterFillForm;
--- pages/u/registerForm.js
+++ pages/u/registerForm.js
@@ -5,13 +5,24 @@ import Header from '@/shared-components/TopicCenter';
 import Form from '@/register/local-components/RegisterFillForm';
 import I18 from '@/core/i18n';
 import '../../static/css/registerForm.css';
+import axios from '@/core/core';
 
 class RegisterForm extends Component {
+
+    static async getInitialProps() {
+        const endDate = await axios.get('https://api-dev.fives.cloud/v0/commons/')
+            .then(response => response.data.result.endDate)
+            .catch(console.log)
+
+        return({endDate});
+    }
+    
     render() {
+        const {endDate} = this.props;
         return(
             <div>
                 <div className="bg-cb-grey-lighter"><Header english={`REGISTER`} thai={`ลงทะเบียน`} englishColor={`text-cb-pink`} borderColor={`border-cb-red`}/></div>
-                <div className="bg-white"><Form /></div>
+                <div className="bg-white"><Form endDate={endDate} /></div>
                 {/* <div className="flex flex-col items-center text-white py-10" style={{ backgroundColor: '#8e9dc0' }}><FacebookButton /></div> */}
                 <FacebookButton />
                 <Footer />

6349214eb25dc8c47b9038e3d2d381960d9b34a3
new5558 <new17353@gmail.com>
2019-02-06 01:27:28 +0700

    added reverse proxy

--- components/core/core.js
+++ components/core/core.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({    
-    baseURL: 'https://jsonplaceholder.typicode.com',
+    baseURL: 'http://localhost:3000/v0',
     withCredentials: true,
 });
 
--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require('express')
 const next = require('next')
 const nextI18NextMiddleware = require('next-i18next/middleware')
 
+const devProxy = {
+  '/v0': {
+    target: 'https://api-dev.fives.cloud/v0/',
+    pathRewrite: { '^/v0': '/' },
+    changeOrigin: true
+  }
+}
+const env = process.env.NODE_ENV
+const dev = env !== 'production'
+
 const nextI18next = require('./components/core/i18n')
 
 const app = next({ dev: process.env.NODE_ENV !== 'production' })
@@ -10,6 +20,13 @@ const handle = app.getRequestHandler();
 (async () => {
   await app.prepare()
   const server = express()
+
+  if (dev && devProxy) {
+    const proxyMiddleware = require('http-proxy-middleware')
+    Object.keys(devProxy).forEach(function (context) {
+      server.use(proxyMiddleware(context, devProxy[context]))
+    })
+  }
 
   nextI18NextMiddleware(nextI18next, app, server)
 

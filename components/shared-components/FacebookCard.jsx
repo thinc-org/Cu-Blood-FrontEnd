b9e3b400356023de3b7d2d5162a53ba0c5cabf07
new5558 <new17353@gmail.com>
2019-03-11 04:23:15 +0700

    facebookcard new layout for medium screen (ipad)

--- components/shared-components/FacebookCard.jsx
+++ components/shared-components/FacebookCard.jsx
@@ -13,9 +13,9 @@ class FacebookCard extends React.Component {
         i18n.language === 'th' ? moment.locale('th') : moment.locale('en');
 
         return (
-            <div className={"bg-white flex flex-col justify-between shadow-md text-black lg:mb-0 " + (hasMarginBottom ? "mb-10" : "mb-0")} style={{ borderRadius: '1rem', width: "300px" }}>
+            <div className={"bg-white flex flex-col justify-between shadow-md text-black lg:mb-0 facebookCard " + (hasMarginBottom ? "mb-10" : "mb-0")}>
                 <div>
-                    <div className="bg-white flex bg-center bg-cover shadow-inner" style={{ height: '300px', borderTopLeftRadius: '1rem', borderTopRightRadius: '1rem', backgroundImage: 'url(' + full_picture + ')', backgroundColor: '#b8c2cc'}}></div>
+                    <div className="bg-white flex bg-center bg-cover shadow-inner facebookImage" style={{backgroundImage: 'url(' + full_picture + ')'}}></div>
                     <div className="flex flex-row items-center px-6 bg-white my-5">
                         <img className="w-8" src='/static/logo/clock.svg' alt="clock" />
                         <p className="pl-2 font-semibold text-lg font-cu-body" style={{ color: '#8e9dc0' }}>

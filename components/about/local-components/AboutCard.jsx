544fc5c4d796f7dc4f1efabd3b2e409dfa818a02
new5558 <new17353@gmail.com>
2019-03-11 23:14:06 +0700

    change display logic in about card

--- components/about/local-components/AboutCard.jsx
+++ components/about/local-components/AboutCard.jsx
@@ -4,39 +4,18 @@ export default class AboutCard extends React.Component {
     render() {
         const nameLength = this.props.englishName;
         const length = nameLength.length;
-
-
-
-        if (length >= 20) {
-            return (
-                <div className="m-4 bg-grey-lightest w-64 flex flex-col items-center text-center font-cu-heading font-medium" style={{ borderRadius: '2rem' }} >
-                    <img src={this.props.picture} aria-hidden alt="User Profile Picture" className=" p-4 h-64 w-64" />
-                    <div className="pb-4 px-2">
-                        <div className="text-2xl">
-                            {this.props.englishName}
-                        </div>
-                        <div className="text-lg">
-                            {this.props.myRole}
-                        </div>
-                    </div>
-                </div>
-            )
-
-        } else {
-            return (
-                <div className="m-4 bg-grey-lightest w-64 flex flex-col items-center text-center font-cu-heading font-medium" style={{ borderRadius: '2rem' }} >
-                    <img src={this.props.picture} aria-hidden alt="User Profile Picture" className=" p-4 h-64 w-64" />
-                    <div className="pb-4 px-2">
-                        <div className="text-2xl">
+        return (
+            <div className="m-4 bg-grey-lightest w-64 flex flex-col items-center text-center font-cu-heading text-2xl font-medium" style={{ borderRadius: '2rem' }} >
+                <img src={this.props.picture} aria-hidden alt="User Profile Picture" className=" p-4 h-64 w-64" />
+                <div className="pb-4 px-2">
+                    <div className={length >= 25 ? 'text-xl' : ''}>
                         {this.props.englishName}
-                        </div>
-                        <div className="text-lg">
-                            {this.props.myRole}
-                        </div>
+                    </div>
+                    <div className="text-lg">
+                        {this.props.myRole}
                     </div>
                 </div>
-            )
-        }
-        
+            </div>
+        )
     }
 }

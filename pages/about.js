256b7aac91ce377fca6a9c01418edbea8ab6db6b
new5558 <new17353@gmail.com>
2019-03-11 22:54:46 +0700

    change person image in about page to svg

--- pages/about.js
+++ pages/about.js
@@ -17,26 +17,27 @@ class About extends React.Component {
 
     render() {
       const { t } = this.props;
+      const image = "/static/icons/md-person.svg";
         return (
             <div>
                 <AboutHeader />
                 <AboutContent/>
                 <PageHeader english={t('staffHeaderSmall')} englishColor="text-pink" thai={t('staffHeaderBig')} borderColor="border-red" />
                 <div className="layout-wide flex flex-wrap flex-row justify-center pb-10">
-                    <AboutCard englishName={t('person1')} myRole={t('role1')} picture="/static/logo/anon.png" />
-                    <AboutCard englishName={t('person2')} myRole={t('role2')} picture="/static/logo/anon.png" />
-                    <AboutCard englishName={t('person3')} myRole={t('role3')} picture="/static/logo/anon.png" />
-                    <AboutCard englishName={t('person4')} myRole={t('role4')} picture="/static/logo/anon.png" />
-                    <AboutCard englishName={t('person5')} myRole={t('role5')} picture="/static/logo/anon.png" />
-                    <AboutCard englishName={t('person6')} myRole={t('role6')} picture="/static/logo/anon.png" />
-                    <AboutCard englishName={t('person7')} myRole={t('role7')} picture="/static/logo/anon.png" />
-                    <AboutCard englishName={t('person8')} myRole={t('role8')} picture="/static/logo/anon.png" />
-                    <AboutCard englishName={t('person9')} myRole={t('role9')} picture="/static/logo/anon.png" />
-                    <AboutCard englishName={t('person10')} myRole={t('role10')} picture="/static/logo/anon.png" />
-                    <AboutCard englishName={t('person11')} myRole={t('role11')} picture="/static/logo/anon.png" />
-                    <AboutCard englishName={t('person12')} myRole={t('role12')} picture="/static/logo/anon.png" />
-                    <AboutCard englishName={t('person13')} myRole={t('role13')} picture="/static/logo/anon.png" />
-                    <AboutCard englishName={t('person14')} myRole={t('role14')} picture="/static/logo/anon.png" />
+                    <AboutCard englishName={t('person1')} myRole={t('role1')} picture={image} />
+                    <AboutCard englishName={t('person2')} myRole={t('role2')} picture={image} />
+                    <AboutCard englishName={t('person3')} myRole={t('role3')} picture={image}  />
+                    <AboutCard englishName={t('person4')} myRole={t('role4')} picture={image} />
+                    <AboutCard englishName={t('person5')} myRole={t('role5')} picture={image} />
+                    <AboutCard englishName={t('person6')} myRole={t('role6')} picture={image} />
+                    <AboutCard englishName={t('person7')} myRole={t('role7')} picture={image} />
+                    <AboutCard englishName={t('person8')} myRole={t('role8')} picture={image} />
+                    <AboutCard englishName={t('person9')} myRole={t('role9')} picture={image} />
+                    <AboutCard englishName={t('person10')} myRole={t('role10')} picture={image} />
+                    <AboutCard englishName={t('person11')} myRole={t('role11')} picture={image} />
+                    <AboutCard englishName={t('person12')} myRole={t('role12')} picture={image} />
+                    <AboutCard englishName={t('person13')} myRole={t('role13')} picture={image} />
+                    <AboutCard englishName={t('person14')} myRole={t('role14')} picture={image} />
                         
                 </div>
                 <FacebookButton />

20b3f2b877873705fa1def1ef8bc21e5cb53bf5a
new5558 <new17353@gmail.com>
2019-03-13 01:08:16 +0700

    add date support on announcement

--- components/announcement/local-components/AnnouncementContent.jsx
+++ components/announcement/local-components/AnnouncementContent.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from '@/core/core';
+import moment from 'moment';
 
 export default class extends Component {
     constructor(props) {
@@ -19,11 +20,11 @@ export default class extends Component {
 
     render() {
         const ReactMarkdown = require('react-markdown')
-        // const input = '## This is a header\n\nAnd this is a paragraph ' + id;
         const { data } = this.state;
         return data ?
             (
                 <div className="layout-narrow pt-8 pb-8 text-xl font-cu-body">
+                    <span>{moment(data.createdAt).format('MMMM Do YYYY')}</span>
                     <h1 className="font-cu-heading font-bold text-3xl sm:text-5xl mb-3">{data.title}</h1>
                     <div className="flex flex-col items-center mb-5">
                         <img

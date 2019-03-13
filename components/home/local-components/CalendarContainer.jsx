1bfd2aa7c61729df66eec5fec0c99154c940f786
new5558 <new17353@gmail.com>
2019-03-14 00:37:37 +0700

    final adjustment on eventCalendar

--- components/home/local-components/CalendarContainer.jsx
+++ components/home/local-components/CalendarContainer.jsx
@@ -5,10 +5,10 @@ import moment from 'moment';
 import I18 from '@/core/i18n';
 
 
- class CalendarContainer extends React.Component {
+class CalendarContainer extends React.Component {
 
     render() {
-        const {t} = this.props;
+        const { t } = this.props;
         const { calendarEvents = [] } = this.props;
         const eventTitle = map(calendarEvents, 'title');
         const majorLocation = map(calendarEvents, 'location');
@@ -21,10 +21,10 @@ import I18 from '@/core/i18n';
                 <div className="layout-wide">
                     <TopicCenter borderColor="border-cb-red" english={t("calendarHeaderSmall")} thai={t("calendarHeaderBig")} englishColor="text-cb-pink" thaiColor="text-black" />
                     <MajorEvent title={eventTitle[0]} location={majorLocation[0]} time={majorTime[0]} startDate={startDate[0]} endDate={endDate[0]} />
-                    <MinorEvent title={eventTitle[1]} startDate={startDate[1]} endDate={endDate[1]} />
-                    <MinorEvent title={eventTitle[2]} startDate={startDate[2]} endDate={endDate[2]} />
-                    <MinorEvent title={eventTitle[3]} startDate={startDate[3]} endDate={endDate[3]} />
-                    <MinorEvent title={eventTitle[4]} startDate={startDate[4]} endDate={endDate[4]} hasLineBreak={false} />
+                    <MinorEvent title={eventTitle[1]} location={majorLocation[1]} startDate={startDate[1]} endDate={endDate[1]} />
+                    <MinorEvent title={eventTitle[2]} location={majorLocation[2]} startDate={startDate[2]} endDate={endDate[2]} />
+                    <MinorEvent title={eventTitle[3]} location={majorLocation[3]} startDate={startDate[3]} endDate={endDate[3]} />
+                    <MinorEvent title={eventTitle[4]} location={majorLocation[4]} startDate={startDate[4]} endDate={endDate[4]} hasLineBreak={false} />
                 </div>
             </div>
         );
@@ -32,21 +32,25 @@ import I18 from '@/core/i18n';
 }
 
 const MinorEvent = (props) => {
-    const { title, hasLineBreak, startDate, endDate } = props;
+    const { title, hasLineBreak, startDate, endDate, location } = props;
     const startDay = moment(startDate).format('DD');
     const endDay = moment(endDate).format('DD');
     const startMonth = moment(startDate).format('MMMM');
     const endMonth = moment(endDate).format('MMMM');
 
-    if (title && startDate && endDate) {
+    if (title && startDate && endDate && location) {
         return (
             <div className="layout-narrow">
                 <div className="flex flex-col sm:flex-row font-cu-heading justify-start py-5">
                     <div className="flex flex-col text-pink-dark items-center" >
                         <CalendarDate startMonth={startMonth} endMonth={endMonth} startDay={startDay} endDay={endDay} />
                     </div>
-                    <div className="flex flex-col text-wrap text-center sm:text-left items-center">
+                    <div className="flex flex-col text-wrap text-center text-left">
                         {title}
+                        <div className="text-pink-dark flex flex-row justify-center sm:justify-start">
+                            <img className="w-5 h-5 mr-4" src='/static/logo/location.png' alt="clock"></img>
+                            {location}
+                        </div>
                     </div>
                 </div>
                 <LineBreak hasLineBreak={hasLineBreak} />
@@ -72,18 +76,13 @@ const MajorEvent = (props) => {
 
         return (
             <div className="bg-white layout-narrow rounded-lg font-cu-heading text-3xl py-8 shadow-md flex flex-col sm:flex-row" style={{ maxWidth: '800px', paddingLeft: '0', paddingRight: '0', marginBottom: '2rem', marginTop: '2rem' }}>
-                    <CalendarDate startMonth={startMonth} endMonth={endMonth} startDay={startDay} endDay={endDay} />
+                <CalendarDate startMonth={startMonth} endMonth={endMonth} startDay={startDay} endDay={endDay} />
                 <div className="flex flex-col px-8 sm:px-0 sm:pr-8 text-wrap w-full ">
                     {title}
                     <div className="flex flex-col sm:flex-row text-base pt-4">
                         <div className="w-1/2 text-pink-dark flex flex-row items-center pb-4 sm:pb-0">
                             <img className="w-14 h-14 mr-4" src='/static/logo/location.png' alt="clock"></img>
                             {location}
-                        </div>
-                        <div className="w-1/2 text-grey-darker flex flex-row items-center">
-                            <img className="w-14 h-14 mr-4" src='/static/logo/greyClock.png' alt="clock"></img>
-                            Time
-                            {time}
                         </div>
                     </div>
                 </div>

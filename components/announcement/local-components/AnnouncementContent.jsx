0f7ffa673fe492702e83433782fbe0aa562f9db0
new5558 <new17353@gmail.com>
2019-03-14 02:23:52 +0700

    announcement seperated page class to function

--- components/announcement/local-components/AnnouncementContent.jsx
+++ components/announcement/local-components/AnnouncementContent.jsx
@@ -1,29 +1,25 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+const ReactMarkdown = require('react-markdown')
 
-export default class extends Component {
-
-    render() {
-        const ReactMarkdown = require('react-markdown')
-        const { data } = this.props;
-        return (
-            <div className="layout-narrow pt-8 pb-8 text-xl font-cu-body">
-                <span>{moment(data.createdAt).format('MMMM Do YYYY')}</span>
-                <h1 className="font-cu-heading font-bold text-3xl sm:text-5xl mb-3">{data.title}</h1>
-                <div className="flex flex-col items-center mb-5">
-                    <img
-                        alt="announcement header"
-                        src={data.displayImage}
-                        style={{
-                            "height": "100%",
-                            "maxHeight": "500px",
-                            "maxWidth": "500px",
-                            "width": "100%"
-                        }}
-                    />
-                </div>
-                <ReactMarkdown source={data.body} />
-            </div >
-        )
-    }
+export default ({ data }) => {
+    return (
+        <div className="layout-narrow pt-8 pb-8 text-xl font-cu-body">
+            <span>{moment(data.createdAt).format('MMMM Do YYYY')}</span>
+            <h1 className="font-cu-heading font-bold text-3xl sm:text-5xl mb-3">{data.title}</h1>
+            <div className="flex flex-col items-center mb-5">
+                <img
+                    alt="announcement header"
+                    src={data.displayImage}
+                    style={{
+                        "height": "100%",
+                        "maxHeight": "500px",
+                        "maxWidth": "500px",
+                        "width": "100%"
+                    }}
+                />
+            </div>
+            <ReactMarkdown source={data.body} />
+        </div >
+    )
 }

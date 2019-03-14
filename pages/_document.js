9b60794296f2b6e6872c16ca712ab4cadc4cbd5c
new5558 <new17353@gmail.com>
2019-03-15 01:33:03 +0700

    add google webmaster verification

--- pages/_document.js
+++ pages/_document.js
@@ -20,6 +20,7 @@ export default class MyDocument extends Document {
           <meta name="msapplication-config" content="/static/icons/browserconfig.xml" />
           <meta name="theme-color" content="#ffffff" />
           
+          <meta name="google-site-verification" content="2p3xIPpK6em68IB4u-Qq2J7rlo2sbvolhjNl6PR8VXA" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="description" content="โครงการที่รณรงค์ให้นิสิตปัจจุบัน นิสิตเก่า คณาจารย์ และบุคลากร ของ จุฬาลงกรณ์มหาวิทยาลัย ร่วมกันบริจาคโลหิตให้แก่สภากาชาดไทย" />
           <meta name="keywords" content="บริจาคเลือด,บริจาคเลือด จุฬา,บริจาคเลือด เข็ม,บริจาคเลือด นานมั้ย,บริจาคเลือด รพ.จุฬา,บริจาคเลือด เวลา,บริจาคเลือด ที่ไหน,บริจาคเลือด กี่ครั้ง" />
